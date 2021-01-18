7a4756eb908b007e62554f609149feb647b5b635
AHADBEK <66939245+AHADBEK@users.noreply.github.com>
2021-01-18 18:48:45 +0500

    1
    1

---
+++ DASTURLASH_script.js
@@ -0,0 +1,15 @@
+left .button = document.getElementById('button');
+
+button.onlick = function() {
+    button.classList.add('active');
+    active = true;
+
+    setTimeout(function() {
+        button.classList.add('success');
+            },3500)
+    
+    setTimeout(function() {
+        button.classList.remove('active');
+        button.classList.remove('success');
+            },5000);
+}
